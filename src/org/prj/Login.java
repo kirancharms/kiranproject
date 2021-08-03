3a924b930cce73fbbe080ef8d0b8515ebbc02569
kirancharms <kirancharms36@gmail.com>
2021-08-03 11:41:33 +0530

    change

--- src/org/prj/Login.java
+++ src/org/prj/Login.java
@@ -1,5 +1,14 @@
 package org.prj;
 
 public class Login {
+	public static void userName() {
+		// TODO Auto-generated method stub
+		System.out.println("master the blaster");
+
+	}
+	public static void main(String[] args) {
+		userName();
+	}
+
 
 }
